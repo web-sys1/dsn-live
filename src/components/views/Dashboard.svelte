8635d2d27c586593a7408ca8f4cb766c0a10ef09
Web Source (SRC) <web.system.management@gmail.com>
2021-12-26 22:34:39 +0200

    upfold repository

--- src/components/views/Dashboard.svelte
+++ src/components/views/Dashboard.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import { Loading } from "carbon-components-svelte";
+  import { Loading, InlineLoading } from "carbon-components-svelte";
+  import { fly } from "svelte/transition";
 
   import Summary from "../Summary.svelte";
   import DishCard from "../DishCard.svelte";
@@ -13,6 +14,7 @@
   let nextRequest: number = 0;
 
   let DSNData: DSNData = null;
+  let error = null;
 
   // Start making requests based on interval
   onMount(() => {
@@ -20,8 +22,10 @@
       if (nextRequest <= 0) {
         nextRequest = requestIntervalUnits;
         DSNData = await getDSNData();
+		console.log('Undefined? ', DSNData === undefined)
       } else {
         nextRequest -= 1;
+        // console.log(genRds(1000, 20000, 100))
       }
     }, 1000);
 
@@ -39,17 +43,29 @@
 
       // Async process the XML response
       return await processDSNResponse(res);
-    } catch {
-      const err = "Error fetching data.";
-      console.error(err);
-      throw new Error(err);
+    } catch (err) {
+      console.error("Error fetching data.");
     }
   }
+  
+  function generateRandom(min, max, step) {
+    const randomNum = min + Math.random() * (max - min);
+    return Math.round(randomNum / step) * step;
+    }
 
-  // const getNewData = () => {
-  //   DSNData = generateMockData();
-  // };
+  function genRds(MIN_RANDOM, MAX_RANDOM, RANDOM_STEP) {
+     return generateRandom(MIN_RANDOM, MAX_RANDOM, RANDOM_STEP);
+  }
+  
+ //  const getNewData = () => {
+ //    DSNData = generateMockData();
+ //  };
+   
+   const reloadDSN = () => {
+     return requestInterval()
+   }
 </script>
+
 
 {#if DSNData}
   <!-- <button
@@ -62,8 +78,29 @@
       <DishCard {dish} updating={nextRequest <= 0} />
     {/each}
   </div>
-{:else}
+{:else if DSNData === null}
   <Loading />
+{/if}
+
+{#if DSNData === undefined}
+  <div class='error-message'>Couldn't fetch results. Check your internet connection. Please wait a moment for the data to retrieve after reconnecting.
+              {#if nextRequest}
+			  <p class="abs-exception right ewl reload"
+                in:fly={{ x: -10, duration: 250 }}
+                out:fly={{ x: 10, duration: 250 }}
+              >
+                Reconnecting: {nextRequest + "s"}
+              </p> 
+			  {:else}
+              <span
+                class="abs right"
+                in:fly={{ x: -10, duration: 250 }}
+                out:fly={{ x: 10, duration: 250 }}
+              >
+                <InlineLoading description="Reconnecting..." />
+              </span>
+			  {/if}
+  </div>
 {/if}
 
 <style>
@@ -74,4 +111,27 @@
     flex-wrap: wrap;
     margin: 20px;
   }
+  
+  .error-message {
+    margin: 0 auto;
+    color: lightcoral;
+    padding: 14px;
+    width: 100%;
+    max-width: 59%;
+    background-color: var(--black-alpha);
+    border-radius: 2px;
+    box-shadow: 0px 1px 2px 3px #dddddd29;
+    text-align: center;
+    font: message-box;
+    position: relative;
+    top: 11px;
+}
+
+   p.abs-exception.right.ewl.reload {
+    margin: -4px auto;
+    padding: 10px;
+    position: relative;
+    top: 8px;
+}
+
 </style>
